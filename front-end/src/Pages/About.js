38f7078e2dbc6efaf4bcfa8ae8717c31722dba5b
nazisuleman <naziasuleman901@gmail.com>
2023-04-28 14:59:28 +0500

    refactoring

---
+++ front-end/src/Pages/About.js
@@ -0,0 +1,7 @@
+import NavBar from "./NavBar";
+
+const About = () => {
+  return <NavBar />;
+};
+
+export default About;
---
+++ front-end/src/Pages/Tokenization.js
@@ -0,0 +1,7 @@
+import NavBar from "./NavBar";
+
+const Tokenization = () => {
+  return <NavBar />;
+};
+
+export default Tokenization;
