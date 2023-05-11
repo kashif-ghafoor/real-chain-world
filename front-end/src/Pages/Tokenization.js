7c8e3e1bfc5c429f3b1753295e42d1fec03a23a2
nazisuleman <naziasuleman901@gmail.com>
2023-05-12 01:04:54 +0500

    remove extra code

--- front-end/src/Pages/Tokenization.js
+++ front-end/src/Pages/Tokenization.js
@@ -1,87 +1,10 @@
 import NavBar from "../Components/NavBar";
 import React from "react";
-import { Formik } from "formik";
-import * as Yup from "yup";
 
-const initialValues = {
-  name: "",
-  email: "",
-  password: "",
-};
-
-const onSubmit = (values) => {
-  console.log(values);
-};
-
-const validationSchema = Yup.object({
-  name: Yup.string().required("Name is required"),
-  email: Yup.string()
-    .email("Invalid email address")
-    .required("Email is required"),
-  password: Yup.string()
-    .min(6, "Password must be at least 6 characters")
-    .required("Password is required"),
-});
 const Tokenization = () => {
   return (
     <>
       <NavBar />
-      <div>
-        <h1>Registration Form</h1>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={onSubmit}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.name}
-              />
-              {touched.name && errors.name ? <div>{errors.name}</div> : null}
-
-              <label htmlFor="email">Email:</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-              />
-              {touched.email && errors.email ? <div>{errors.email}</div> : null}
-
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-              />
-              {touched.password && errors.password ? (
-                <div>{errors.password}</div>
-              ) : null}
-
-              <button type="submit">Submit</button>
-            </form>
-          )}
-        </Formik>
-      </div>
     </>
   );
 };
