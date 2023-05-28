a5d1b2939873785714e127f5a33d8383416c5147
nazisuleman <naziasuleman901@gmail.com>
2023-05-29 01:46:09 +0500

    created the dasboard

---
+++ front-end/src/Pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard bg-gray-200 p-4" style={{ width: "80%" }}>
+      <h1 className="text-xl font-bold mb-4">Welcome to the Dashboard</h1>
+      {/* Your main dashboard content goes here */}
+    </div>
+  );
+};
+
+export default Dashboard;
