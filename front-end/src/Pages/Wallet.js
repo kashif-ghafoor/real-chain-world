f98b5e88ad80b9e84256583811e4506b9b11fa8f
nazisuleman <naziasuleman901@gmail.com>
2023-05-29 01:47:21 +0500

    created the wallet page

---
+++ front-end/src/Pages/Wallet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Wallet = () => {
+  return (
+    <div className="wallet bg-gray-200 p-4" style={{ width: "80%" }}>
+      <h1 className="text-xl font-bold mb-4">Connect to Wallet</h1>
+      {/* Your main dashboard content goes here */}
+    </div>
+  );
+};
+
+export default Wallet;
