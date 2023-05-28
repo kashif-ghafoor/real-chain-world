6a183d9c356dc062ee40f63fa02c09d42b7f894c
nazisuleman <naziasuleman901@gmail.com>
2023-05-29 01:46:40 +0500

    created the edit profile page

---
+++ front-end/src/Pages/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="profile bg-gray-200 p-4" style={{ width: "80%" }}>
+      <h1 className="text-xl font-bold mb-4">Welcome to the Profile</h1>
+      {/* Your main dashboard content goes here */}
+    </div>
+  );
+};
+
+export default Profile;
