36c5e431b47a2807a6c345ca3fd80aff7b6b1870
kashif-ghafoor <kashifghafoor140@gmail.com>
2023-07-06 01:41:07 +0500

    adds tests for new design of contracts

--- back-end/test/propertyfactory.test.js
+++ back-end/test/propertyfactory.test.js
@@ -1,49 +1,157 @@
 const PropertyFactory = artifacts.require("PropertyFactory");
 const Property = artifacts.require("Property");
 
-contract("PropertyFactory", accounts => {
+contract("PropertyFactory", ([deployer, user, ...accounts]) => {
+  let propertyFactory;
+  const propertyDescription = "Property 1 description";
+  const propertyAddress = "Property 1 address";
+  const initialPrice = 500; // let's say 500 wei
+  const supply = 1000; // let's say 1000 tokens
+  let requestId;
 
-  const ownerAccount = accounts[0];
+  before(async function () {
+    propertyFactory = await PropertyFactory.new(deployer);
+  });
 
-  const property1 = {
-    address : "Property 1 address",
-    description : "Property 1 description",
-    price : 100,
-    supply : 10
-  }
+  it("User should create a new property tokenization request", async function () {
+    const response = await propertyFactory.createPropertyTokenizeRequest(
+      propertyAddress,
+      propertyDescription,
+      initialPrice,
+      supply,
+      { from: user }
+    );
+    requestId = response.logs[0].args.id; // Assuming id is the first argument in the NewPropertyValidationRequested event
+    assert.equal(response.logs[0].event, "NewPropertyValidationRequested", "Wrong event triggered");
+  });
 
-  let factoryInstance
-
-  beforeEach(async () => {
-    factoryInstance = await PropertyFactory.new()
-  })
-
-  describe('Creating new property', () => {
-    it("Should deploy new property", async () => {
-      let listBeforeCreatingNewProperty = await factoryInstance.getProperties();
-      await factoryInstance.createProperty(property1.address, property1.description, property1.price, property1.supply, {from: ownerAccount});
-      let listAfterCreatingNewProperty = await factoryInstance.getProperties();
-
-      assert.equal(listAfterCreatingNewProperty.length, listBeforeCreatingNewProperty.length + 1, "Address is not added successfully");
+  it("Admin should see the requests and suggest a price", async function () {
+    const suggestedPrice = 1000; // let's say 1000 wei
+    const response = await propertyFactory.suggestPrice(requestId, suggestedPrice, {
+      from: deployer,
     });
+    assert.equal(response.logs[0].event, "PriceSuggested", "Wrong event triggered");
+  });
 
-    it("New property info should be equal to the sent params", async () => { 
-      await factoryInstance.createProperty(property1.address, property1.description, property1.price, property1.supply, {from: ownerAccount});
-      let propertiesList = await factoryInstance.getProperties();
-      let newPropertyAddress = propertiesList[0];
-      let property = await Property.at(newPropertyAddress);
-      let propertyInfo = await property.propertyInfo()
-      let propertyAddress = propertyInfo._address;
-      let propertyDescrption = propertyInfo._description;
-      let propertyPrice = propertyInfo.price;
-      let propertySupply = await property.totalSupply();
-  
-      assert.equal(property1.price, propertyPrice, "Wrong property's price");
-      assert.equal(property1.supply, propertySupply, "Wrong property's supply");
-      assert.equal(property1.address, propertyAddress, "Wrong property's address");
-      assert.equal(property1.description, propertyDescrption, "Wrong property's description");
-    });
-  })
-  
+  it("User should accept the suggested price", async function () {
+    const response = await propertyFactory.acceptPrice(requestId, { from: user });
+    assert.equal(response.logs[0].event, "PriceAccepted", "Wrong event triggered");
+  });
 
+  it("Admin should create the property", async function () {
+    const response = await propertyFactory.createProperty(requestId, { from: deployer });
+    assert.equal(response.logs[0].event, "NewPropertyAdded", "Wrong event triggered");
+
+    const propertyAddress = response.logs[0].args.property; // Assuming property address is the second argument in the NewPropertyAdded event
+    const property = await Property.at(propertyAddress);
+    const propertyInfo = await property.propertyInfo();
+
+    // Here you can perform more checks on the property contract if needed
+    const propertyDescription = propertyInfo._description;
+    assert.equal(
+      propertyDescription,
+      "Property 1 description",
+      "Property description does not match"
+    );
+
+    const propertyOwner = await property.owner();
+    assert.equal(propertyOwner, user, "Property owner does not match");
+  });
+
+  it("owner should have one property now", async function () {
+    const properties = await propertyFactory.getPropertiesOfOwner(user);
+    assert.equal(properties.length, 1, "One property should exist for the owner now");
+  });
 });
+
+// contract("PropertyFactory", (accounts) => {
+//   const ownerAccount = accounts[0];
+//   const companyAccount = accounts[1];
+//   console.log("company Account", companyAccount);
+
+//   const property1 = {
+//     address: "Property 1 address",
+//     description: "Property 1 description",
+//     price: 100,
+//   };
+//   let factoryInstance;
+
+//   beforeEach(async () => {
+//     // create factory Instance and pass companyAccount parameter to constructor
+//     factoryInstance = await PropertyFactory.new(companyAccount);
+//   });
+
+//   describe("Property Tokenization Request", () => {
+//     it("Should create a new property tokenization request", async () => {
+//       let listBeforeRequest = await factoryInstance.getPropertyRequestsByOwner(ownerAccount);
+//       await factoryInstance.createPropertyTokenizeRequest(
+//         property1.address,
+//         property1.description,
+//         { from: ownerAccount }
+//       );
+//       let listAfterRequest = await factoryInstance.getPropertyRequestsByOwner(ownerAccount);
+
+//       assert.equal(
+//         listAfterRequest.length,
+//         listBeforeRequest.length + 1,
+//         "Property tokenization request was not created successfully"
+//       );
+//     });
+
+//     it("New property tokenization request info should be equal to the sent params", async () => {
+//       await factoryInstance.createPropertyTokenizeRequest(
+//         property1.address,
+//         property1.description,
+//         { from: ownerAccount }
+//       );
+//       let propertiesList = await factoryInstance.getPropertyRequestsByOwner(ownerAccount);
+//       let newRequest = propertiesList[0];
+
+//       assert.equal(property1.address, newRequest.propertyInfo._address, "Wrong property's address");
+//       assert.equal(
+//         property1.description,
+//         newRequest.propertyInfo._description,
+//         "Wrong property's description"
+//       );
+//     });
+
+//     it("Company can suggest price", async () => {
+//       await factoryInstance.createPropertyTokenizeRequest(
+//         property1.address,
+//         property1.description,
+//         { from: ownerAccount }
+//       );
+
+//       let propertiesList = await factoryInstance.getPropertyRequestsByOwner(ownerAccount);
+//       let newRequest = propertiesList[0];
+//       console.log("newRequest id", newRequest.propertyInfo.id);
+
+//       await factoryInstance.suggestPrice(newRequest.propertyInfo.id, property1.price, {
+//         from: companyAccount,
+//       });
+//       let updatedRequest = (await factoryInstance.getPropertyRequestsByOwner(ownerAccount))[0];
+//       console.log("updatedRequest", updatedRequest);
+
+//       assert.equal(property1.price, updatedRequest.suggestedPrice, "Wrong suggested price");
+//     });
+
+//     it("Owner can accept suggested price", async () => {
+//       await factoryInstance.createPropertyTokenizeRequest(
+//         property1.address,
+//         property1.description,
+//         { from: ownerAccount }
+//       );
+//       let propertiesList = await factoryInstance.getPropertyRequestsByOwner(ownerAccount);
+//       let newRequest = propertiesList[0];
+
+//       await factoryInstance.suggestPrice(newRequest.propertyInfo.id, property1.price, {
+//         from: companyAccount,
+//       });
+//       await factoryInstance.acceptPrice(newRequest.propertyInfo.id, { from: ownerAccount });
+
+//       let updatedRequest = (await factoryInstance.getPropertyRequestsByOwner(ownerAccount))[0];
+
+//       assert.equal(property1.price, updatedRequest.propertyInfo.price, "Price was not accepted");
+//     });
+//   });
+// });
